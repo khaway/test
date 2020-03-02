0e982c649411177f154011caf97d56f46bdb6a0c
KINGER <oimasterok@gmail.com>
2020-03-02 22:27:19 +0300

    new_3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,17 @@
 window.$ = require('jquery')
+require('slick-carousel')
 
-alert("123")
+
+$(document).ready(function() {
+    $('.slider').slick({
+        dots: true,
+        lazyLoad: 'ondemand',
+        // prevArrow: $('.reviews-slider-wrapper .slider-prev'),
+        // nextArrow: $('.reviews-slider-wrapper .slider-next')
+    })
+
+    $('#sidebar-btn').on('click', function() {
+        $('#sidebar').toggleClass('visible');
+    });
+});
+
