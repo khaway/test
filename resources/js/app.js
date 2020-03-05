fbf3cf47d75ded41bba0a835678e4806127df223
KINGER <oimasterok@gmail.com>
2020-03-05 22:23:52 +0300

    Banksy3

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ window.$ = require('jquery')
 
 $(document).ready(function() {
     $('#sidebar-btn, #sidebar-btn2').on('click', function() {
+        $('#SidenavToggler').find('img').toggle();
         $('#sidebar').toggleClass('visible hidden');
     });
 });
